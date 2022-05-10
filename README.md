215d7525d70bf7ad83f9e8a047ad7e81d68d72a4
kosta4310 <realanka77@gmail.com>
2022-05-10 22:44:58 +0300

    init: start test project

--- README.md
+++
@@ -1,3 +0,0 @@
-## [rsschool-cv](https://kosta4310.github.io/rsschool-cv/cv)
-
-timestamp
--- kosta4310-JSFE2022Q1/shelter-dom/src/js/pages.js
+++
@@ -1,6 +0,0 @@
-export class Page {
-  constructor(n, k) {
-    this.n = n; /*номер страницы */
-    this.k = k; /*количество фото на странице */
-  }
-}
